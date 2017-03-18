5cc434573806df294144d7c8987b3ca0f8031a72
wd4219 <57272276@qq.com>
2017-03-18 16:54:22 +0800

    部分样式修改

--- src/components/body/good-list-item.vue
+++ src/components/body/good-list-item.vue
@@ -1,101 +1,112 @@
 <template>
-  <div class="good-list-item clearfix" @click="routerTo">
-   <div class="good-list-item-left">
-     <img src="../../common/images/meishi.png" alt="商家图片" class="goods-pic">
-   </div>
-   <div class="good-list-item-right">
-     <div class="main">
-       <h2 class="good-name">吉野家（北京站店）<span class="distance">1.2km</span></h2>
-       <div class="score"><span class="star"></span><span class="star"></span><span class="star"></span><span class="star"></span><span class="star"></span><span  class="sell-number">月售 1378 单</span><span class="send-time">29分钟</span></div>
-      <div class="send-detail"><span class="send-price">起送价￥30</span><span class="send-money">配送费￥10</span></div>
-     </div>
-     <div class="discount">
-       <p class="discount-item"><img src="../../common/images/decrease_3@2x.png" alt="" class="discount-icon">满30减10；满50减18；满80减28</p>
-     </div>
-   </div>
+  <div class="good-list-item" @click="routerTo">
+    <div class="good-list-item-left">
+      <img src="../../common/images/meishi.png" alt="商家图片" class="goods-pic">
+    </div>
+    <div class="good-list-item-right">
+      <div class="main">
+        <h2 class="good-name">吉野家（北京站店）<span class="distance">1.2km</span></h2>
+        <div class="score"><span class="sell-number">月售 1378 单</span><span class="send-time">29分钟</span></div>
+        <div class="send-detail"><span class="send-price">起送价￥30</span><span class="send-money">配送费￥10</span></div>
+      </div>
+      <div class="discount">
+        <p class="discount-item"><img src="../../common/images/decrease_3@2x.png" alt="" class="discount-icon">满30减10；满50减18；满80减28</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'good-list-item',
-  data () {
-    return {
-      
-    };
-  },
-  methods:{
-    routerTo:function(){
-      this.$emit('go');
+  import Star from '../component/star'
+  export default {
+    name: 'good-list-item',
+    data() {
+      return {
+        score: 4
+      };
+    },
+    methods: {
+      routerTo: function () {
+        this.$emit('go');
+      }
+    },
+    components: {
+      'v-star': Star
     }
-  }
-};
+  };
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-    .good-list-item{
-        width: 100%;
-        background-color: #fff;
-    }
-    .good-list-item .good-list-item-left{
-      float: left;
-      padding-right: 8px; 
-    }
-    .good-list-item .good-list-item-right{
-      margin-left: 110px;
-    }
-    .good-list-item .good-list-item-right .main{
-      padding-right: 10px;
-      border-bottom: 1px solid #ccc;
-      padding-bottom: 10px;
-      margin-bottom: 10px;
-    }
-    .good-list-item .good-list-item-right .good-name{
-      font-size: 16px;
-      line-height: 32px;
-    }
-    .good-list-item .good-list-item-right .good-name .distance{
-      font-size: 12px;
-      float: right;
-    }
-    .good-list-item-right .score{
-      font-size: 12px;
-      padding: 0 0px 8px;
-    }
-    .good-list-item-right .score .star{
-      display: inline-block;
-      width: 12px;
-      height: 12px;
-      background: url(../../common/images/star24_on@2x.png) no-repeat center center;
-      background-size: 12px;
-      margin-left: 2px;
-    }
-    .good-list-item-right .score .sell-number{
-      padding-left: 10px;
-    }
-    .good-list-item-right .score .send-time{
-      float: right;
-    }
-    .good-list-item-right .send-detail{
-      font-size: 14px;
-    }
-    .good-list-item-right .send-detail .send-price{
-      margin-right: 10px;
-      padding-right: 10px;
-      border-right: 1px solid #ccc;
-    }
-    .good-list-item .goods-pic{
-      width: 100px;
-      height: 60px;
-      border-right: 1px solid #ccc;
-      border-bottom: 1px solid #ccc;
-    }
-    .discount .discount-item{
-      font-size: 12px;
-      line-height: 24px;
-    }
-    .discount .discount-item .discount-icon{
-      width: 12px;
-    }
+  .good-list-item {
+    width: 100%;
+    background-color: #fff;
+    display: flex;
+  }
+  .good-list-item .good-list-item-left {
+    margin-top: 8px;
+    flex: 0 0 70px;
+    padding:0 8px;
+  }
+  
+  .good-list-item .good-list-item-right {
+    flex: 1;
+  }
+  
+  .good-list-item .good-list-item-right .main {
+    padding-right: 10px;
+    border-bottom: 1px solid #ccc;
+    padding-bottom: 10px;
+    margin-bottom: 6px;
+  }
+  
+  .good-list-item .good-list-item-right .good-name {
+    font-size: 16px;
+    line-height: 32px;
+  }
+  
+  .good-list-item .good-list-item-right .good-name .distance {
+    font-size: 12px;
+    float: right;
+  }
+  
+  .good-list-item-right .score {
+    font-size: 12px;
+    padding: 0 0px 8px;
+  }
+  
+  .good-list-item-right .score .sell-number {
+    padding-left: 10px;
+  }
+  
+  .good-list-item-right .score .send-time {
+    float: right;
+  }
+  
+  .good-list-item-right .send-detail {
+    font-size: 14px;
+  }
+  
+  .good-list-item-right .send-detail .send-price {
+    margin-right: 10px;
+    padding-right: 10px;
+    border-right: 1px solid #ccc;
+  }
+  
+  .good-list-item .goods-pic {
+    width: 60px;
+    height: 60px;
+    border-radius: 2px;
+  }
+  
+  .discount .discount-item {
+    font-size: 12px;
+    line-height: 24px;
+  }
+  
+  .discount .discount-item .discount-icon {
+    width: 12px;
+  }
+
 </style>
--- src/components/shop/goods.vue
+++ src/components/shop/goods.vue
@@ -1,43 +1,31 @@
 <template>
   <div class="goods">
     <div class="goods-content">
-      <div class="goods-left-side">
-      <ul class="goods-left-side-list" v-el="">
-        <li class="goods-left-side-list-item on"><span class="text"><span class="icon"></span>热销榜</span>
-        </li>
-        <li class="goods-left-side-list-item"><span class="text"><span class="icon"></span>单人特色经典套餐</span>
-        </li>
-        <li class="goods-left-side-list-item"><span class="text"><span class="icon"></span>单人特色经典套餐</span>
-        </li>
-        <li class="goods-left-side-list-item"><span class="text"><span class="icon"></span>单人特色经典套餐</span>
-        </li>
-        <li class="goods-left-side-list-item"><span class="text"><span class="icon"></span>单人特色经典套餐</span>
-        </li>
-        <li class="goods-left-side-list-item"><span class="text"><span class="icon"></span>单人特色经典套餐</span>
-        </li>
-        <li class="goods-left-side-list-item"><span class="text"><span class="icon"></span>单人特色经典套餐</span>
-        </li>
-        <li class="goods-left-side-list-item"><span class="text"><span class="icon"></span>单人特色经典套餐</span>
-        </li>
-        <li class="goods-left-side-list-item"><span class="text"><span class="icon"></span>单人特色经典套餐</span>
-        </li>
-        <li class="goods-left-side-list-item"><span class="text"><span class="icon"></span>单人特色经典套餐</span>
-        </li>
-        <li class="goods-left-side-list-item"><span class="text"><span class="icon"></span>单人特色经典套餐</span>
-        </li>
-        <li class="goods-left-side-list-item"><span class="text"><span class="icon"></span>单人特色经典套餐</span>
-        </li>
-        <li class="goods-left-side-list-item"><span class="text"><span class="icon"></span>单人特色经典套餐</span>
-        </li>
-        <li class="goods-left-side-list-item"><span class="text"><span class="icon"></span>单人特色经典套餐</span>
-        </li>
-      </ul>
-    </div>
-    <div class="goods-right-content">
-      <v-goodlistitem></v-goodlistitem>
-      <v-goodlistitem></v-goodlistitem>
-      <v-goodlistitem></v-goodlistitem>
-    </div>
+      <div class="goods-left-side" ref="menuWrapper">
+        <ul class="goods-left-side-list">
+          <li class="goods-left-side-list-item" :class="{'on':index == currentIndex}" v-for="(item,index) in goods" @click="selectMenu(index,$event)"><span class="text"><span class="icon"></span>{{item.name}}</span>
+          </li>
+        </ul>
+      </div>
+      <div class="goods-right-content" ref="foodsWrapper">
+        <ul>
+          <li v-for="item in goods" ref="foodList">
+            <h2 class="goods-item-list-title">{{item.name}}</h2>
+            <ul class="goods-item-list-content">
+              <li v-for="food in item.foods">
+                <div class="item-icon"><img :src="food.icon" alt=""></div>
+                <div class="item-description">
+                  <h3 class="item-title">{{food.name}}</h3>
+                  <div class="item-name">{{food.description}}</div>
+                  <div class="sell-number"><span class="">月售{{food.sellCount}}份</span><span class="good-evaluation-rate
+">好评率{{food.rating}}%</span></div>
+                  <div class="price"><span class="now"><i>￥</i>{{food.price}}</span><span class="old" v-show="food.oldPrice"><i>￥</i>{{food.oldPrice}}</span></div>
+                </div>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
     </div>
     <div class="goods-shopcart">
       <div class="shopcart-totalprice"><span class="iconfont icon-shopcart"></span>￥{{total_price}}</div>
@@ -48,21 +36,80 @@
 </template>
 <script>
   import GoodsListItem from '../component/goods-item-list'
-
+  import BScroll from 'better-scroll'
+  import axios from 'axios'
   export default {
     name: 'goods',
     data() {
       return {
-        total_price:30
+        total_price: 30,
+        goods: [],
+        listHeight:[],
+        scrollY:0,
       }
     },
-    computed:{
-      littleprice(){
-        return this.total_price > 20 ? '去结算':'￥20起送';
+    computed: {
+      littleprice() {
+        return this.total_price > 20 ? '去结算' : '￥20起送';
+      },
+      currentIndex(){
+        for(let i = 0;i < this.listHeight.length;i++){
+          let height1 = this.listHeight[i];
+          let height2 = this.listHeight[i+1];
+
+          if(!height2 || (this.scrollY >= height1 && this.scrollY < height2)){
+            return i;
+          }
+        }
+        return 0;
+      }
+    },
+    methods: {
+      selectMenu(index, event) {
+        if (!event._constructed) {
+          return;
+        }
+        let foodList = this.$refs.foodList;
+        let el = foodList[index];
+        this.foodsScroll.scrollToElement(el, 300);
+      },
+      _initScroll() {
+        this.menuScroll = new BScroll(this.$refs.menuWrapper, {
+          click: true
+        });
+        this.foodsScroll = new BScroll(this.$refs.foodsWrapper, {
+          click: true,
+          probeType: 3
+        });
+        this.foodsScroll.on('scroll', (pos) => {
+          this.scrollY = Math.abs(Math.round(pos.y));
+        })
+      },
+      _calculateHeight() {
+        let foodList = this.$refs.foodList;
+        let height = 0;
+        this.listHeight.push(height);
+        for (let i = 0; i < foodList.length; i++) {
+          let item = foodList[i];
+          height += item.clientHeight;
+          this.listHeight.push(height);
+        }
       }
     },
     components: {
       'v-goodlistitem': GoodsListItem
+    },
+    created() {
+      let self = this;
+      axios.get('../../../static/data.json').then(function (response) {
+        self.goods = response.data.goods;
+        self.$nextTick(function() {
+          self._initScroll();
+          self._calculateHeight();
+        });
+      }).catch(function (error) {
+        console.log('请求出错了');
+      });
     }
   }
 
@@ -81,7 +128,6 @@
     flex: 0 0 80px;
     width: 80px;
     background-color: #f3f5f7;
-    overflow-y: auto;
   }
   
   .goods-left-side-list-item {
@@ -118,73 +164,79 @@
   
   .goods-right-content {
     flex: 1;
-    overflow-y: auto;
+  }
+  
+  .goods-shopcart {
+    display: flex;
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    height: 46px;
+    line-height: 46px;
+    background-color: #141d27;
+    color: rgba(255, 255, 255, 0.4);
+  }
+  
+  .goods-shopcart .icon-shopcart {
+    position: absolute;
+    top: -10px;
+    left: 12px;
+    display: inline-block;
+    width: 50px;
+    height: 50px;
+    text-align: center;
+    border-radius: 50%;
+    background-color: #141d27;
+  }
+  
+  .goods-shopcart .icon-shopcart:before {
+    display: inline-block;
+    position: absolute;
+    width: 40px;
+    height: 40px;
+    line-height: 40px;
+    border-radius: 50%;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background: rgba(255, 255, 255, 0.2);
+    font-size: 25px;
+  }
+  
+  .goods-shopcart .shopcart-totalprice {
+    flex: 0 0 115px;
+    width: 115px;
+    padding-left: 60px;
+    text-align: center;
+    font-size: 16px;
+    color: rgba(255, 255, 255, 0.4);
+    font-weight: 700;
+    box-sizing: border-box;
+  }
+  
+  .goods-shopcart .shopcart-send-money {
+    flex: 1;
+    font-size: 10px;
+    padding-left: 12px;
+    margin-left: 12px;
+    line-height: 22px;
+    height: 22px;
+    margin-top: 12px;
+    border-left: 1px solid rgba(255, 255, 255, 0.1);
+  }
+  
+  .goods-shopcart .littleprice {
+    flex: 0 0 110px;
+    width: 110px;
+    font-size: 12px;
+    font-weight: 700;
+    background: rgba(255, 255, 255, 0.2);
+    text-align: center;
+  }
+  
+  .goods-shopcart .cansend {
+    background: #00b43c;
+    color: #fff;
   }
 
-  .goods-shopcart{
-    display:flex;
-    position:absolute;
-    bottom:0;
-    width:100%;
-    height:46px;
-    line-height:46px;
-    background-color:#141d27;
-    color:rgba(255,255,255,0.4);
-  }
-  .goods-shopcart .icon-shopcart{
-    position:absolute;
-    top:-10px;
-    left:12px;
-    display:inline-block;
-    width:50px;
-    height:50px;
-    text-align:center;  
-    border-radius:50%;
-    background-color:#141d27;
-  }
-  .goods-shopcart .icon-shopcart:before{
-    display:inline-block;
-    position:absolute;
-    width:40px;
-    height:40px;
-    line-height:40px;
-    border-radius:50%;
-    top:50%;
-    left:50%;
-    transform:translate(-50%,-50%);
-    background:rgba(255,255,255,0.2);
-    font-size:25px;
-  }
-  .goods-shopcart .shopcart-totalprice{
-    flex:0 0 115px;
-    width:115px;
-    padding-left:60px;
-    text-align:center;
-    font-size:16px;
-    color:rgba(255,255,255,0.4);
-    font-weight:700;
-    box-sizing:border-box;
-  }
-   .goods-shopcart .shopcart-send-money{
-     flex:1;
-     font-size:10px;
-     padding-left:12px;
-     margin-left:12px;
-     line-height:22px;
-     height:22px;
-     margin-top:12px;
-     border-left:1px solid rgba(255,255,255,0.1);
-   }
-   .goods-shopcart .littleprice{
-     flex:0 0 110px;
-     width:110px;
-     font-size:12px;
-     font-weight:700;
-     background:rgba(255,255,255,0.2);
-     text-align:center;
-   }
-   .goods-shopcart .cansend{
-     background:#00b43c;
-     color:#fff;
-   }
 </style>
